e10d5d2fcf139643ae041df0287dd84f35f9d800
Justin Dennison <justin1dennison@gmail.com>
2016-10-17 19:22:41 -0400

    added logging middleware

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 
 var express = require('express');
 var server = express();
+var logger = require('./middleware/logger.js');
 
 var port = process.env.PORT || 8080;
 
 server.use(express.static(__dirname + '/public'));
+server.use(logger);
 
 server.get('/', function(request, response){
   response.sendFile('public/html/index.html', {root: __dirname});
