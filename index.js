a7d53dcc6a8601a9416bb45c18df3209ca75d981
Justin Dennison <justin1dennison@gmail.com>
2016-10-17 19:40:00 -0400

    added cors dependency

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
 var express = require('express');
 var server = express();
 var logger = require('./middleware/logger.js');
+var cors = require('cors');
 
 var port = process.env.PORT || 8080;
 
 server.use(express.static(__dirname + '/public'));
 server.use(logger);
+server.use(cors());
 
 server.get('/', function(request, response){
   response.sendFile('public/html/index.html', {root: __dirname});
