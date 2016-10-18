ce7b63a53967c1d822fccd0d6d6ac16f91f822a1
Justin Dennison <justin1dennison@gmail.com>
2016-10-18 19:01:42 -0400

    refactor forecast to a separate router

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ var express = require('express');
 var server = express();
 var logger = require('./middleware/logger.js');
 var cors = require('cors');
-var axios = require('axios');
-var apiKey = require('./config.js').apiKey;
+
+
+var forecastRouter = require('./routers/forecast.router.js');
 
 var port = process.env.PORT || 8080;
 
@@ -16,21 +17,8 @@ server.use(cors());
 server.get('/', function(request, response){
   response.sendFile('public/html/index.html', {root: __dirname});
 });
+server.use(forecastRouter);
 
-server.get('/forecast/:latitude,:longitude', function(request, response){
-  var url = 'https://api.darksky.net/forecast/'+ apiKey +'/'
-  + request.params.latitude +','+ request.params.longitude;
-  var timeoutConfig = {
-    timeout: 2000
-  };
-  axios.get(url, timeoutConfig)
-        .then(function(forecast){
-          response.send(forecast.data);
-        })
-        .catch(function(error){
-          response.send(error);
-        });
-});
 
 server.listen(port, function(){
   console.log('Now listening on port', port);
