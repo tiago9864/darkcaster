7b64af5d52df7c7765c7785af89e49f2cca8484c
Justin Dennison <justin1dennison@gmail.com>
2016-10-17 20:56:21 -0400

    added api hiding and latitude

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var express = require('express');
 var server = express();
 var logger = require('./middleware/logger.js');
 var cors = require('cors');
+var axios = require('axios');
+var apiKey = require('./config.js').apiKey;
 
 var port = process.env.PORT || 8080;
 
@@ -13,6 +15,20 @@ server.use(cors());
 
 server.get('/', function(request, response){
   response.sendFile('public/html/index.html', {root: __dirname});
+});
+
+server.get('/forecast/:latitude,:longitude', function(request, response){
+  var url = 'https://api.darksky.net/forecast/'+ apiKey +'/'+ request.params.latitude +',-122.4233';
+  var timeoutConfig = {
+    timeout: 2000
+  };
+  axios.get(url, timeoutConfig)
+        .then(function(forecast){
+          response.send(forecast.data);
+        })
+        .catch(function(error){
+          response.send(error);
+        });
 });
 
 server.listen(port, function(){
