4662c3809627cf140373e124c53a1c942134dcba
Justin Dennison <justin1dennison@gmail.com>
2016-10-18 20:04:20 -0400

    added authorization middleware

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express');
 var server = express();
 var logger = require('./middleware/logger.js');
 var cors = require('cors');
-
+var authorize = require('./middleware/auth.js');
 
 var forecastRouter = require('./routers/forecast.router.js');
 
@@ -13,6 +13,7 @@ var port = process.env.PORT || 8080;
 server.use(express.static(__dirname + '/public'));
 server.use(logger);
 server.use(cors());
+server.use(authorize);
 
 server.get('/', function(request, response){
   response.sendFile('public/html/index.html', {root: __dirname});
